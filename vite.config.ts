8b89d657a7cf49683eb7ff15fa41289b35e46ff0
Gustavo Farias <gustavocel82@gmail.com>
2023-07-03 00:24:02 -0300

    remove vite compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression({deleteOriginFile: true})],
+  plugins: [react()],
 })
